77f60d59bd5e57f91c255fbfd1bd50e553c1460f
jahidhasan3737094 <jahidhasansaif094@gmail.com>
2024-02-18 18:53:57 -0500

    bug1 fixed confirmRegistration

--- src/main/java/com/experment/learningacademy/service/StudentService.java
+++ src/main/java/com/experment/learningacademy/service/StudentService.java
@@ -7,6 +7,8 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class StudentService {
     @Autowired
@@ -14,22 +16,23 @@ public class StudentService {
     @Autowired
     //private StudentRepository studentRepository;
     private JavaMailSender mailSender;
-    public String registerStudent(Student student){
+
+    public String registerStudent(Student student) {
 
         this.studentRepository.save(student);
 
-        String toEmail=student.getEmail();
+        String toEmail = student.getEmail();
         System.out.println(student.getStudentId());
 
-        String text="hi Mr."+student.getFirstName() + " Thank you for registration,please confirm" +
+        String text = "hi Mr." + student.getFirstName() + " Thank you for registration,please confirm" +
                 "your registrtion by click on the below link ";
-        String link=" http://localhost:8080/"+"confirm/"+student.getStudentId();
+        String link = " http://localhost:8080/" + "confirm/" + student.getStudentId();
 
-        SimpleMailMessage message=new SimpleMailMessage();
+        SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom("jahidhasansaif095@gmail.com");
         message.setTo(toEmail);
         message.setSubject("mail for confirmation registration.");
-        message.setText(text+link);
+        message.setText(text + link);
         this.mailSender.send(message);
 
         return "mail sent successfully";
@@ -37,10 +40,17 @@ public class StudentService {
 
     public String confirmRegistration(Integer confirmToken) {
 
-        Student student=this.studentRepository.findById(confirmToken).get();
-        student.setRegistrationConfirmation(true);
-        this.studentRepository.save(student);
-        System.out.println("confirmation");
-        return "confirmation successfull... now you can login...";
+        Optional<Student> optionalStudent = this.studentRepository.findById(confirmToken);
+
+        if (optionalStudent.isPresent()) {
+            Student student = optionalStudent.get();
+            student.setRegistrationConfirmation(true);
+            this.studentRepository.save(student);
+            System.out.println("Confirmation");
+            return "Confirmation successful... now you can login...";
+        } else {
+            System.out.println("Student with confirmation token " + confirmToken + " not found");
+            return "Student with confirmation token " + confirmToken + " not found";
+        }
     }
 }
